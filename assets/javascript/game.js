f6f03f300f36658aa78c41d0901001cf6bbce8e9
Dave Nolan <dpn.nolan@gmail.com>
2019-10-20 15:30:12 -0400

    added var html to be updated with guesses and a decreasing guesses left counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,11 +13,25 @@ document.onkeyup = function() {
 
     console.log(psychicGuess);
 
-    if (userguess!="a" || userguess!="b" || userguess!="c" ||userguess!="d" || userguess!="e" || userguess!="f" || userguess!="g" || userguess!="h" || userguess!="i" || userguess!="j" || userguess!="k" || userguess!="l" || userguess!="m" || userguess!="n" || userguess!="o" || userguess!="p" || userguess!="q" || userguess!="r" || userguess!="s" || userguess!="t" || userguess!="u" || userguess!="v" || userguess!="w" || userguess!="x"|| userguess!="y" || userguess!="z") {
+    if ((userguess!="a" || userguess!="b" || userguess!="c" ||userguess!="d" || userguess!="e" || userguess!="f" || userguess!="g" || userguess!="h" || userguess!="i" || userguess!="j" || userguess!="k" || userguess!="l" || userguess!="m" || userguess!="n" || userguess!="o" || userguess!="p" || userguess!="q" || userguess!="r" || userguess!="s" || userguess!="t" || userguess!="u" || userguess!="v" || userguess!="w" || userguess!="x"|| userguess!="y" || userguess!="z")
+    ) {
        alert("please choose a letter!");
         }
         if ((userguess == psychicGuess)) {
+            wins++;
             alert("you win!")
         }
+
+        else ((userguess !== psychicGuess)) 
+            chances--;
         
+        
+        var html = "<h1>The Psychic Game</h1>" +
+        "<h5>Guess what letter I am thinking of</h5>" +
+        "<p> Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Gueses Left: " + chances + "</p>";
+
+        document.querySelector("#game").innerHTML = html;
+
     }
