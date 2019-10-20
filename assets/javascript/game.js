bd6b02d25893d107b5e26289626a9b6e5108ca3d
Dave Nolan <dpn.nolan@gmail.com>
2019-10-20 14:26:45 -0400

    added alert for userguess being anything else but a letter but its currently broken

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,4 +10,9 @@ document.onkeyup = function() {
 
     console.log(psychicGuess);
 
+    if (userguesss!=='a' || usergueess!=='b' || usergueess!=='c' || usergueess!=='d' || usergueess!=='e' || usergueess!=='f' || usergueess!=='g' || usergueess!=='h' || usergueess!=='i' || usergueess!=='j' || usergueess!=='k' || usergueess!=='l' || usergueess!=='m' || usergueess!=='n' || usergueess!=='o' || usergueess!=='p' || usergueess!=='q' || usergueess!=='r' || usergueess!=='s' || usergueess!=='t' || usergueess!=='u' || usergueess!=='v' || usergueess!=='w' || usergueess!=='x' || usergueess!=='y' || usergueess!=='z') {
+        alert("please choose a letter!");
+    } else {
+        alert("chosen correctly!");
+    }
 }
