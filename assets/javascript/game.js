ce74121ffb56aa3f814d05d2556a1df84a482adc
Dave Nolan <dpn.nolan@gmail.com>
2019-10-20 15:15:56 -0400

    added wins losses and chances variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,7 @@
 var options = ["a", "b", "c", "d"];
+var wins = 0;
+var losses = 0;
+var chances = 10;
 
 document.onkeyup = function() {
     var userguess = String.fromCharCode(event.keyCode).
@@ -11,12 +14,10 @@ document.onkeyup = function() {
     console.log(psychicGuess);
 
     if (userguess!="a" || userguess!="b" || userguess!="c" ||userguess!="d" || userguess!="e" || userguess!="f" || userguess!="g" || userguess!="h" || userguess!="i" || userguess!="j" || userguess!="k" || userguess!="l" || userguess!="m" || userguess!="n" || userguess!="o" || userguess!="p" || userguess!="q" || userguess!="r" || userguess!="s" || userguess!="t" || userguess!="u" || userguess!="v" || userguess!="w" || userguess!="x"|| userguess!="y" || userguess!="z") {
-        alert("you chose a letter");
+       alert("please choose a letter!");
+        }
         if ((userguess == psychicGuess)) {
             alert("you win!")
-        }else{
-            alert("please choose a letter!");
         }
         
     }
-}
