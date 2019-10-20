706182caf9a505f65b4d0f4ba884d09fdc7c22f7
Dave Nolan <dpn.nolan@gmail.com>
2019-10-20 14:09:28 -0400

    added 4 options for for computer to choose from randomly upon keyevent

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,13 @@
+var options = ["a", "b", "c", "d"];
+
 document.onkeyup = function() {
     var usergueess = String.fromCharCode(event.keyCode).
     toLowerCase();
 
     console.log(usergueess);
+
+    var psychicGuess = options[Math.floor(Math.random()*options.length)];
+
+    console.log(psychicGuess);
 
 }
