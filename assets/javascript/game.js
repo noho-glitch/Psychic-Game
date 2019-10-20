cc4edd6e099422571fe6f655d8c5d9f4c5986546
Dave Nolan <dpn.nolan@gmail.com>
2019-10-20 19:15:56 -0400

    fixed please choose a letter bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,7 @@ document.onkeyup = function() {
     console.log(psychicGuess);
 
     if (
-        (userguess!=="a" || userguess!="b" || userguess!="c" ||userguess!="d" || userguess!="e" || userguess!="f" || userguess!="g" || userguess!="h" || userguess!="i" || userguess!="j" || userguess!="k" || userguess!="l" || userguess!="m" || userguess!="n" || userguess!="o" || userguess!="p" || userguess!="q" || userguess!="r" || userguess!="s" || userguess!="t" || userguess!="u" || userguess!="v" || userguess!="w" || userguess!="x"|| userguess!="y" || userguess!="z")
+        (userguess!=="a" && userguess!="b" && userguess!="c" &&userguess!="d" && userguess!="e" && userguess!="f" && userguess!="g" && userguess!="h" && userguess!="i" && userguess!="j" && userguess!="k" && userguess!="l" && userguess!="m" && userguess!="n" && userguess!="o" && userguess!="p" && userguess!="q" && userguess!="r" && userguess!="s" && userguess!="t" && userguess!="u" && userguess!="v" && userguess!="w" && userguess!="x"&& userguess!="y" && userguess!="z")
     ) {
        alert("please choose a letter!");
         }
