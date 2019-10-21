6213bea53c22dad26e367e37c871f89e11c3bd13
Dave Nolan <dpn.nolan@gmail.com>
2019-10-21 10:04:17 -0400

    added the rest of the alphabet to var options for computer choices

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var options = ["a", "b", "c", "d"];
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var chances = 10;
